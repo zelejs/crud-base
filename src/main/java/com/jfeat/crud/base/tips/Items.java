6fa4de96a51e34702e67cda7994483a0a820a1d4
vincent@pc <vincent@jfeat.com>
2018-12-20 14:42:17 +0800

    add Items

---
+++ src/main/java/com/jfeat/crud/base/tips/Items.java
@@ -0,0 +1,18 @@
+package com.jfeat.crud.base.tips;
+
+import java.util.List;
+
+/**
+ * Created by vincenthuang on 21/10/2017.
+ */
+public class Items {
+    private List<String> items;
+
+    public List<String> getItems() {
+        return items;
+    }
+
+    public void setItems(List<String> items) {
+        this.items = items;
+    }
+}
