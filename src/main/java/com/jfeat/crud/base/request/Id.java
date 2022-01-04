c30125aada780c80f1e1a48089d8b1bd49a5865e
m.2@sandbox <m.2@sandbox>
2022-01-04 17:54:35 +0800

    add Id

---
+++ src/main/java/com/jfeat/crud/base/request/Id.java
@@ -0,0 +1,13 @@
+package com.jfeat.crud.base.request;
+
+public class Id {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
