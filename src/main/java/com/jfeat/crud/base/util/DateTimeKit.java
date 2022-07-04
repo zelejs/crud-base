e96895c957ff12d4eb80c2fb2e8c0b947300a660
‘ZuoyeSir’ <1152808759@qq.com>
2022-07-04 17:42:28 +0800

    修改时间逻辑

--- src/main/java/com/jfeat/crud/base/util/DateTimeKit.java
+++ src/main/java/com/jfeat/crud/base/util/DateTimeKit.java
@@ -4,7 +4,12 @@ import java.util.Date;
 
 public class DateTimeKit {
 
+    //    最大显示3天 1小时*24*3
+    public static final long MAX_TIME = 3600 * 24 * 3;
+
     public static String toTimeline(Date time){
+
+
         long curTime =System.currentTimeMillis() / (long) 1000 ;
 
         Long timeStamp = time.getTime()/(long)1000;
@@ -14,20 +19,16 @@ public class DateTimeKit {
         if (resultTime < 60 && resultTime >= 0) {
             return "刚刚";
 
-        } else if (resultTime >= 60 && resultTime <60*2) {
+        } else if (resultTime >= 60 && resultTime < 3600) {
 
-            return "1分钟前";
+            return resultTime / 60 + "分钟前";
 
-        } else if (resultTime >= 60*2 && resultTime < 3600 * 2) {
+        } else if (resultTime >= 3600 && resultTime < 3600 * 24) {
 
-            return "2小时前";
+            return resultTime / 3600 + "小时前";
 
-        } else if (resultTime >= 3600 * 2 && resultTime < 3600 * 24 * 3) {
-
-            return "3天前";
-        }
-        else if (resultTime >= 3600 * 24 * 3 && resultTime < 3600 * 24 * 7) {
-            return "7天前";
+        } else if (resultTime >= 3600 * 24 && resultTime < MAX_TIME) {
+            return resultTime / 3600 / 24 + "天前";
         }
 
 //        } else if (resultTime >= 3600 * 24 * 30 && resultTime < 3600 * 24 * 30 * 12) {
