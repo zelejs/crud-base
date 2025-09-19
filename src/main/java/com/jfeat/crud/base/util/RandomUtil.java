06bb1d2ff6284ee02c708226fdb7bcd64a1168b2
Mac-Mini <ehuhaim@mac-mini.local>
2025-09-19 11:52:25 +0800

    enhance: update RandomUtil

--- src/main/java/com/jfeat/crud/base/util/RandomUtil.java
+++ src/main/java/com/jfeat/crud/base/util/RandomUtil.java
@@ -2,39 +2,74 @@ package com.jfeat.crud.base.util;
 import java.security.SecureRandom;
 
 public class RandomUtil {
-    /**
-     * 安全随机数生成器
-     */
+
     private final static SecureRandom random = new SecureRandom();
+    
+    /**
+     * 获取随机字符串 (基数字符串为大写字母+小写字母+数字）
+     * @param length 生成的随机字符串长度
+     * @return 随机字符串
+     */
+    public static String getRandomStr_Base62(int length){
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return getRandomStr(length, str);    
+    }
 
     /**
-     * 返回一段指定长度的随机字符串
-     *
+     * 获取随机字符串 (基数字符串为小写字段+数字）
+     * @param length 生成的随机字符串长度
+     * @return 随机字符串
+     */
+    public static String getRandomStr_Base36(int length){
+        final String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        return getRandomStr(length, base);    
+    }
+
+    /**
+     * 获取随机字符串 (基字符串为小写字段+娄字）
+     * @param length 生成的随机字符串长度
+     * @return 随机字符串
+     */
+    public static String getRandomStr(int length){
+        final String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        return getRandomStr(length, base);
+    }
+
+    /** 
      * @param length 长度
      * @return 随机字符串
      */
-    public static String getRandomStr(int length) {
-        String base = "abcdefghijklmnopqrstuvwxyz0123456789.";
+    private static String getRandomStr(int length, String baseString) {
         StringBuilder randomNumber = new StringBuilder();
-
         for(int i = 0; i < length; ++i) {
-            int number = random.nextInt(base.length());
-            randomNumber.append(base.charAt(number));
+            int number = random.nextInt(baseString.length());
+            randomNumber.append(baseString.charAt(number));
         }
 
         return randomNumber.toString();
     }
 
     /**
-     * 获取随机数字符串
+     * 获取随机数字字符串
      * @param length
      * @return
      */
-    public static String getRandomCode(int length) {
+    public static String getRandomNum(int length){
         StringBuilder randomNumber = new StringBuilder();
         for (int i = 0; i < length; i++) {
             randomNumber.append(random.nextInt(9));
         }
         return randomNumber.toString();
+    }
+
+
+    /**
+     * 获取随机数字字符串
+     * @param length
+     * @return
+     */
+    @Deprecated
+    public static String getRandomCode(int length) {
+        return getRandomNum(length);
     }
 }
